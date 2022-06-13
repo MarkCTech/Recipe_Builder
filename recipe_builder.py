ce65dc3cf35af20efe9ba5f5c6ed09715c9e9e12
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-06-13 09:42:19 -0500

    Added comments, and a table for daily logging

--- recipe_builder.py
+++ recipe_builder.py
@@ -8,29 +8,29 @@ class AddData:
         self.ingr_list = ingr_list
         self.macros = macros
 
-    def add_deets(self, name, servings, ingr):
+    def add_deets(self, *args):
         print("Add SQL values to database")
 
 
 class AddRecipe(AddData):
-    def add_deets(self, name: str, servings: str, rec_ingr: str):
+    def add_deets(self, *args):
+        # Connect to database, insert a row of data and close
         con = set_con()
         cur = con.cursor()
-        # Insert a row of data
         cur.execute(
             "INSERT INTO recipes VALUES (?,?,?)",
-            (name, servings, rec_ingr))
+            (args[0], args[1], args[2]))
         end_con(con)
 
 
 class AddIngr(AddData):
-    def add_deets(self, ingredient: str, grams: str, macro: tuple):
+    def add_deets(self, *args):
+        # Connect to database, insert a row of data and close
         con = set_con()
         cur = con.cursor()
-        # Insert a row of data
         cur.execute(
             "INSERT INTO ingredients VALUES (?,?,?,?,?,?,?,?)",
-            (ingredient, grams, macro[0], macro[1], macro[2], macro[3], macro[4], macro[5]))
+            (args[0], args[1], args[2][0], args[2][1], args[2][2], args[2][3], args[2][4], args[2][5]))
         end_con(con)
 
 
@@ -39,17 +39,17 @@ class Searcher:
         self.name = name
 
     def search(self, name):
-        pass
+        print("Returns database query")
 
 
 class SearchRec(Searcher):
     def search(self, name):
+        # Connect to database, searches the name of a row of data and close
         con = set_con()
         cur = con.cursor()
         try:
             cur.execute("select * from recipes where Recipe=:name", {"name": name})
             return cur.fetchall()
-
         except ValueError:
             return [None]
         finally:
@@ -58,6 +58,7 @@ class SearchRec(Searcher):
 
 class SearchIngr(Searcher):
     def search(self, name):
+        # Connect to database, searches the name of a row of data and close
         con = set_con()
         cur = con.cursor()
         try:
@@ -119,68 +120,76 @@ def set_db():
     cur.execute('''CREATE TABLE IF NOT EXISTS recipes (Recipe,Servings,Ingredients)''')
     cur.execute('''CREATE TABLE IF NOT EXISTS ingredients (Ingredient,Grams,Calories,Protein,Carbs,Satfat,Unsat,
     Fibre)''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS log (Day, Time, Recipe, Calories)''')
     # Commit the changes and close
     con.commit()
     con.close()
 
 
 def choose(res_list):
+    # Presents User with list of search results from recipe or ingredient query
+    # And asks them to pick one, or enter 0 for none
     print(f"\nAre any of these what you're looking for? {res_list}")
     length = len(res_list)
     while True:
         try:
             x = int(input("\nEnter item number, or 0 for no: "))
+            if type(x) is int:
+
+                if x > length:
+                    print(f'Choice must be below {length}')
+                elif x == 0:
+                    return False
+                else:
+                    return x
+                break
         except ValueError:
             print('Must be integer, Try Again!')
-        else:
-            if x > length:
-                print(f'Choice must be below {length}')
-            elif x == 0:
-                x = False
-                return x
-            else:
-                return x
-        break
 
 
 def main():
     set_db()
     play_check = True
     while play_check:
+        # Search for recipe in database, User choose which or none
         rec_name = input("\nEnter recipe name to search for: ")
-        rec_ingr = []
-
         srch_rec = SearchRec(rec_name)
         srch_rec_res = srch_rec.search(rec_name)
         choice = choose(srch_rec_res)
-
+        rec_ingr = []
+        # If choice is a valid index, recipe log is saved to variable
         if choice is int:
             result = srch_rec_res[choice]
             print("\nRecipe found!")
             print(result)
 
         if not choice:
+            # If recipe not in database, User adds the details
             print("\nRecipe not found, please add it yourself!")
-
-            ingr_check = True
-            while ingr_check:
+            # Controls state of play
+            ingr_play = True
+            while ingr_play:
+                # Search for ingredient, User choose which or none
                 ingr_name = input("\nEnter Ingredient to add to recipe: ")
-
                 srch_ingr = SearchIngr(ingr_name)
                 srch_ingr_res = srch_ingr.search(ingr_name)
                 choice = choose(srch_ingr_res)
-
+                # If choice is valid index, ingredient is added to recipe ingredients rec_ingr
                 if choice is int:
                     result = srch_rec_res[choice]
                     print("\nIngredient found!")
                     print(result)
                     rec_ingr.append(result)
+
                 if not choice:
+                    # If ingredient not in database, User adds macros
                     grams = input("Serving in Grams: ")
                     macros = add_ingr()
+                    # Adds ingredient to database
                     ingredient = AddIngr(ingr_name, grams, macros)
                     ingredient.add_deets(ingr_name, grams, macros)
-                ingr_check = ask_another("ingredient")
+                # Asks if user is finished adding ingredients to the recipe
+                ingr_play = ask_another("ingredient")
 
         servings = str(input(f"How many servings of {rec_name}? "))
         recipe = AddRecipe(rec_name, servings, str(rec_ingr))
